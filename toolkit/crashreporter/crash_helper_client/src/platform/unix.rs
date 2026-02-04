51f21fd5cddddbfc185a2f98414b689472cff8b2
Gabriele Svelto <gsvelto@mozilla.com>
2026-02-04 09:45:37 +0000

    Bug 2002791 - Check that the first step of the crash helper launch was executed correctly r=afranchuk
    We spawn a new process which launches the crash helper and exits
    immediately, in order for the latter to be daemonized. Prior to this
    patch we waited on said process to return and assumed that it did
    successfully, this could hide potential startup failures. Our code would
    detect that the helper did not launch at a later stage and returned an
    error that would make it difficult to identify the root cause. With this
    change if the first step of the crash helper launch fails we know
    immediately that it did and why.
    
    Differential Revision: https://phabricator.services.mozilla.com/D279893

--- toolkit/crashreporter/crash_helper_client/src/platform/unix.rs
+++ toolkit/crashreporter/crash_helper_client/src/platform/unix.rs
@@ -2,11 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use crash_helper_common::{ignore_eintr, BreakpadChar, BreakpadData, IPCChannel, IPCConnector};
 use nix::{
     spawn::{posix_spawn, PosixSpawnAttr, PosixSpawnFileActions},
-    sys::wait::waitpid,
+    sys::wait::{waitpid, WaitStatus},
     unistd::getpid,
 };
 use std::{
@@ -24,6 +24,16 @@ impl CrashHelperClient {
     ) -> Result<CrashHelperClient> {
         let channel = IPCChannel::new()?;
         let (_listener, server_endpoint, client_endpoint) = channel.deconstruct();
+        // SAFETY: `program` is guaranteed to point to a valid nul-terminated
+        // string by the caller.
+        let program = unsafe { CStr::from_ptr(program) };
+        // SAFETY: `breakpad_data` is guaranteed to point to a valid
+        // nul-terminated string by the caller.
+        let breakpad_data =
+            unsafe { CString::from_vec_unchecked(breakpad_data.to_string().into_bytes()) };
+        // SAFETY: `minidump_path` is guaranteed to point to a valid
+        // nul-terminated string by the caller.
+        let minidump_path = unsafe { CStr::from_ptr(minidump_path) };
 
         CrashHelperClient::spawn_crash_helper(
             program,
@@ -39,18 +49,14 @@ impl CrashHelperClient {
     }
 
     fn spawn_crash_helper(
-        program: *const BreakpadChar,
-        breakpad_data: BreakpadData,
-        minidump_path: *const BreakpadChar,
-        endpoint: IPCConnector,
+        program: &CStr,
+        breakpad_data: CString,
+        minidump_path: &CStr,
+        server_endpoint: IPCConnector,
     ) -> Result<()> {
         let parent_pid = getpid().to_string();
         let parent_pid_arg = unsafe { CString::from_vec_unchecked(parent_pid.into_bytes()) };
-        let program = unsafe { CStr::from_ptr(program) };
-        let breakpad_data_arg =
-            unsafe { CString::from_vec_unchecked(breakpad_data.to_string().into_bytes()) };
-        let minidump_path = unsafe { CStr::from_ptr(minidump_path) };
-        let endpoint_arg = endpoint.serialize()?;
+        let endpoint_arg = server_endpoint.serialize()?;
 
         let file_actions = PosixSpawnFileActions::init()?;
         let attr = PosixSpawnAttr::init()?;
@@ -67,7 +73,7 @@ impl CrashHelperClient {
             &[
                 program,
                 &parent_pid_arg,
-                &breakpad_data_arg,
+                &breakpad_data,
                 minidump_path,
                 &endpoint_arg,
             ],
@@ -76,9 +82,13 @@ impl CrashHelperClient {
 
         // The child should exit quickly after having forked off the
         // actual crash helper process, let's wait for it.
-        ignore_eintr!(waitpid(pid, None))?;
+        let status = ignore_eintr!(waitpid(pid, None))?;
 
-        Ok(())
+        if let WaitStatus::Exited(_, _) = status {
+            Ok(())
+        } else {
+            bail!("The crash helper process failed to start and exited with status: {status:?}");
+        }
     }
 
     #[cfg(not(target_os = "linux"))]
