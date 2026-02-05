ca95b1b912f303c0204d1655053d3c009cc8881f
Bryan Thrall <bthrall@mozilla.com>
2026-02-05 21:45:23 +0000

    Bug 1780686 - Extract standalone base types from Glean types r=nbp,toolkit-telemetry-reviewers,chutten
    SpiderMonkey needs to build without Gecko types like nsCString and nsTArray. It
    can use the extracted base classes.
    
    Not all Glean types have standalone base classes, because it would require a
    larger effort to make them useful without Gecko types. For example,
    DatetimeMetric would require SpiderMonkey include PRTime, and StringMetric
    would need to handle SpiderMonkey string types.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266845

--- toolkit/components/glean/bindings/GleanMetric.h
+++ toolkit/components/glean/bindings/GleanMetric.h
@@ -7,6 +7,7 @@
 #ifndef mozilla_glean_GleanMetric_h
 #define mozilla_glean_GleanMetric_h
 
+#include "mozilla/glean/bindings/CommonStandalone.h"
 // The following include provides GleanWebidlEnabled(), used by generated
 // GleanMetricsBinding.cpp
 #include "mozilla/glean/bindings/GleanWebIDL.h"
@@ -21,8 +22,6 @@ enum HistogramID : uint32_t;
 }  // namespace mozilla::Telemetry
 
 namespace mozilla::glean {
-
-typedef uint64_t TimerId;
 
 class GleanMetric : public nsISupports, public nsWrapperCache {
  public:
